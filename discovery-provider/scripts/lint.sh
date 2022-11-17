4c5fd9a375f6f51d1752c2c1367c20330ee9f007
endline <endline>
2022-11-17 17:22:43 +0000

    MOAR WIP

--- discovery-provider/scripts/lint.sh
+++ discovery-provider/scripts/lint.sh
@@ -11,11 +11,11 @@ fi
 set -ex
 
 cd $PROTOCOL_DIR
-flake8 discovery-provider/src/
-flake8 discovery-provider/integration_tests/
-isort ${ISORT_FLAG} discovery-provider/src/
-isort ${ISORT_FLAG} discovery-provider/alembic/
-isort ${ISORT_FLAG} discovery-provider/integration_tests/
-black ${BLACK_FLAG} discovery-provider/src/
-black ${BLACK_FLAG} discovery-provider/integration_tests/
-# mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers discovery-provider/src/
+flake8 /audius-discovery-provider/src/
+flake8 /audius-discovery-provider/integration_tests/
+isort ${ISORT_FLAG} /audius-discovery-provider/src/
+isort ${ISORT_FLAG} /audius-discovery-provider/alembic/
+isort ${ISORT_FLAG} /audius-discovery-provider/integration_tests/
+black ${BLACK_FLAG} /audius-discovery-provider/src/
+black ${BLACK_FLAG} /audius-discovery-provider/integration_tests/
+# mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers /audius-discovery-provider/src/
